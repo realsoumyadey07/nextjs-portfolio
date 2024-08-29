c71f0ab0e11454dac395e8f5f8971b36ad11ee15
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-08-29 08:29:59 +0530

    skill section started

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -18,81 +18,84 @@ const Contact = () => {
   };
   return (
     <div id="Contact">
-    <div className="max-w-md w-full mx-auto border-none rounded-none md:rounded-2xl p-4 md:p-8 bg-transparent dark:bg-transparent">
-      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Contact with Soumyadip
-      </h2>
-      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Submit the follwing form inorder to get in touch with me
-      </p>
+      <div  className="max-w-md w-full mx-auto border-none rounded-none md:rounded-2xl p-4 md:p-8 bg-transparent dark:bg-transparent">
+        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+          Contact with Soumyadip
+        </h2>
+        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+          Submit the follwing form inorder to get in touch with me
+        </p>
 
-      <form className="my-8" onSubmit={handleSubmit}>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-          <LabelInputContainer>
-            <Label htmlFor="fullname">Full name</Label>
-            <Input id="fullname" placeholder="Ram" type="text" />
+        <form className="my-8" onSubmit={handleSubmit}>
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+            <LabelInputContainer>
+              <Label htmlFor="fullname">Full name</Label>
+              <Input id="fullname" placeholder="Ram" type="text" />
+            </LabelInputContainer>
+
+          </div>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">Email Address</Label>
+            <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
           </LabelInputContainer>
-          
-        </div>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
-        </LabelInputContainer>
-        
-        <LabelInputContainer className="mb-8">
-          <Label htmlFor="twitterpassword">Leave a massage</Label>
-          <Input
-            className="h-[100px]  dark:bg-transparent"
-            id="message"
-            placeholder="Write something here"
-            type="text"
-          />
-        </LabelInputContainer>
 
-        <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-          type="submit"
-        >
-          Send &rarr;
-          <BottomGradient />
-        </button>
+          <LabelInputContainer className="mb-8">
+            <Label htmlFor="twitterpassword">Leave a massage</Label>
+            <Input
+              className="h-[100px]"
+              id="message"
+              placeholder="Write something here"
+              type="text"
+            />
+          </LabelInputContainer>
 
-        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+          <button
+            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            type="submit"
+          >
+            Send &rarr;
+            <BottomGradient />
+          </button>
 
-        <div className="flex flex-col space-y-4">
-          <Link
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            href={"https://github.com/realsoumyadey07"}
-          >
-            <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              GitHub
-            </span>
-            <BottomGradient />
-          </Link>
-          <Link
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            href={"https://www.linkedin.com/in/soumyadip-dey-01782a251/"}
-          >
-            <IconBrandLinkedin className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              Linkedin
-            </span>
-            <BottomGradient />
-          </Link>
-          <Link
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            href={"https://x.com/Realsoumyadey"}
-          >
-            <IconBrandX className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              X
-            </span>
-            <BottomGradient />
-          </Link>
-        </div>
-      </form>
-    </div>
+          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+          <div className="flex flex-col space-y-4">
+            <Link
+              className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+              href={"https://github.com/realsoumyadey07"}
+            >
+              <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+              <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                GitHub
+              </span>
+              <BottomGradient />
+            </Link>
+            <Link
+              className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+              href={"https://www.linkedin.com/in/soumyadip-dey-01782a251/"}
+            >
+              <IconBrandLinkedin className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+              <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                Linkedin
+              </span>
+              <BottomGradient />
+            </Link>
+            <Link
+              className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+              href={"https://x.com/Realsoumyadey"}
+            >
+              <IconBrandX className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+              <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                X
+              </span>
+              <BottomGradient />
+            </Link>
+          </div>
+        </form>
+      </div>
+      <div className="w-full flex justify-center items-center h-[70px]">
+        <p className="font-sans">© Copyright by The Developer 2024</p>
+      </div>
     </div>
   );
 }
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,11 +1,50 @@
+"use client"
 import React from 'react'
-import { Element } from 'react-scroll'
+import { HoverEffect } from './ui/card-hover-effect';
 
 const Skills = () => {
+  const projects = [
+    {
+      title: "Reactjs",
+      description:
+        "A technology company that builds economic infrastructure for the internet.",
+      link: "https://stripe.com",
+    },
+    {
+      title: "TypeScript",
+      description:
+        "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+      link: "https://netflix.com",
+    },
+    {
+      title: "Nextjs",
+      description:
+        "A multinational technology company that specializes in Internet-related services and products.",
+      link: "https://google.com",
+    },
+    {
+      title: "Node.js",
+      description:
+        "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+      link: "https://meta.com",
+    },
+    {
+      title: "MongoDB",
+      description:
+        "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+      link: "https://amazon.com",
+    },
+    {
+      title: "Express.js",
+      description:
+        "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
+      link: "https://microsoft.com",
+    },
+  ];
   return (
-    <div id="Skills">
-    <div className='h-screen'>
-      Skills
+    <div id="Skills" className='min-h-screen'>
+    <div className="max-w-5xl mx-auto px-8">
+      <HoverEffect items={projects} />
     </div>
     </div>
   )
