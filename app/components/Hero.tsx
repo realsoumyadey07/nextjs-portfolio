bfc23971b43123821603d997df3cb2d078a4fcff
Soumyadip Dey <soumyadipdey802@gmail.com>
2025-01-27 22:09:04 +0530

    cv updated

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -66,14 +66,14 @@ const Hero  = () => {
           <TextGenerateEffect  className='mt-5 text-sm' words='I am a passionate React Developer from India, currently pursuing final year of BCA from Maulana Abul Kalam Azad University of Technology.Full Stack Developer with a strong focus on modern web technologies, including React, Next.js, TypeScript, Node.js, JavaScript, and MongoDB. Passionate about building scalable, high-performance web applications with a deep understanding of front-end and back-end development. Eager to contribute to dynamic teams and continuously enhance skills in full stack development.' />
 
           <div className='my-7'>
-            <Link to={"/Projects"} smooth={true} duration={500} spy={true} offset={-80} >
+            <a href="/assets/Soumyadip_Deys_resume.pdf" download="Soumyadip_Dey_Resume.pdf">
               <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-                  Go to Projects
+                  Download CV
                 </span>
               </button>
-            </Link>
+            </a>
           </div>
           
 
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,96 +1,75 @@
-"use client"
-import React from 'react'
-import Image from 'next/image'
-import { BackgroundGradient } from './ui/background-gradient'
+"use client";
+import React from 'react';
+import Image from 'next/image';
+import { BackgroundGradient } from './ui/background-gradient';
 import { FaArrowRightLong } from 'react-icons/fa6';
 import { images } from '../constants';
 import Link from 'next/link';
-import { TextGenerateEffect } from './ui/text-generate-effect';
 
 const projectData = [
   {
     title: "Curepulse",
-    description: "Curepulse is a healthcare management web application using Nextjs, Appwrite cloud.",
+    description:
+      "Curepulse is a healthcare management web application using Nextjs, Appwrite cloud.",
     image: images.projectFirstImage,
-    projectLink: "https://curepulse-nextjs.vercel.app/"
+    projectLink: "https://curepulse-nextjs.vercel.app/",
   },
   {
-    title: "LMS - Learning Management System",
-    description: "A learning management system for selling and buying courses using nextjs, tailwindCss redux, redis, express and mongodb.",
+    title: "MERN - Chat Application",
+    description:
+      "A Fullstack MERN stack Chat Application for messages, file sharing and video calls using nextjs, tailwindCss redux, redis, express and mongodb.",
     image: images.projectSecondImage,
-    projectLink: "https://github.com/realsoumyadey07/LMS-backend-nodejs"
+    projectLink: "https://github.com/realsoumyadey07/MERN-Chat-App",
   },
   {
     title: "Aora - a video streaming mobile app",
-    description: "A video streaming mobile app using React Native , appwrite cloud, nativewind",
+    description:
+      "A video streaming mobile app using React Native, appwrite cloud, nativewind",
     image: images.projectThirdImage,
-    projectLink: "https://github.com/realsoumyadey07/Aora-react-native"
+    projectLink: "https://github.com/realsoumyadey07/Aora-react-native",
   },
-]
+];
 
 const Projects = () => {
   return (
-    <div id="Projects">
-      <div className='w-[95%] lg:w-[92%] lg:flex-row m-auto min-h-screen flex flex-col justify-between container '>
-        {
-          projectData && projectData.map((i) => (
-            <div key={i.title} className='lg:w-[394px] h-auto my-6'>
-              <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+    <div id="Projects" className="py-10">
+      <div className="w-[95%] lg:w-[92%] mx-auto min-h-screen flex flex-col">
+        <div className="grid gap-8 px-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          {projectData.map((project) => (
+            <div
+              key={project.title}
+              className="w-full h-full flex flex-col items-center justify-between"
+            >
+              <BackgroundGradient className="rounded-[22px] p-4 sm:p-8 bg-white dark:bg-zinc-900 shadow-md flex flex-col justify-between">
                 <Image
-                  src={i.image}
-                  alt={i.title}
-                  height="400"
-                  width="400"
-                  className="object-contain"
+                  src={project.image}
+                  alt={project.title}
+                  height="240"
+                  width="240"
+                  className="object-contain rounded-md mx-auto"
                 />
-                <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-                  {i.title}
+                <p className="text-lg font-semibold text-black mt-4 dark:text-neutral-200">
+                  {project.title}
                 </p>
 
-                <p className="text-sm text-neutral-600 dark:text-neutral-400">
-                  {i.description}
+                <p className="text-sm text-neutral-600 dark:text-neutral-400 mt-2">
+                  {project.description}
                 </p>
-                
-                <Link href={i.projectLink} className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 w-20 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+
+                <Link
+                  href={project.projectLink}
+                  className="rounded-full px-4 py-2 text-white flex items-center justify-center bg-black mt-4 text-xs font-bold dark:bg-zinc-800 hover:scale-105 transition-all"
+                >
                   <span>Link </span>
-                  <span className=" rounded-full text-[0.6rem] px-2 py-0 text-white">
-                    <FaArrowRightLong />
-                  </span>
+                  <FaArrowRightLong className="ml-2 text-sm" />
                 </Link>
               </BackgroundGradient>
             </div>
-          ))
-        }
-
-        {/* <div className='lg:w-[394px] h-auto'>
-          <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
-            <Image
-              src={`/public/assets/project-1.png`}
-              alt="curepulse"
-              height="400"
-              width="400"
-              className="object-contain"
-            />
-            <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-              Air Jordan 4 Retro Reimagined
-            </p>
-
-            <p className="text-sm text-neutral-600 dark:text-neutral-400">
-              The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-              February 17, 2024. Your best opportunity to get these right now is by
-              entering raffles and waiting for the official releases.
-            </p>
-            <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-              <span>Buy now </span>
-              <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-                $100
-              </span>
-            </button>
-          </BackgroundGradient>
-        </div> */}
+          ))}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
